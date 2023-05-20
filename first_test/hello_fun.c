d2a906ca2687cbdf988da299b869c1932052cc9c
tzyrrxr <sjlin0827@outlook.com>
2023-05-20 23:35:29 +0800

    Revert "first update"
    This reverts commit f474c54c35d49eca4888dda0d61f0602cab2c5e9.

--- first_test/hello_fun.c
+++
@@ -1,5 +0,0 @@
-#include "hello_fun.h"
-
-void hello (const char *name) {
- printf("%s\n", name);
-}
--- first_test/hello_world.c
+++
@@ -1,6 +0,0 @@
-#include "hello_fun.h"
-
-int main(int argc, char *argv[]) {
- hello ("I want to test");
- return 0;
-}
