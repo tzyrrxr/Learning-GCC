4a52c17ea9eeabf41798f901a458e9ed324a5fb1
tzyrrxr <sjlin0827@outlook.com>
2023-05-26 08:20:08 +0800

    2023-05-26 tmp

---
+++ ch7/test.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+ string str1 = "string 1";
+ string str2 = "string 2";
+ std::cout << str1 << std::endl;
+ std::cout << str2 << std::endl;
+ return 0;
+}
---
+++ ch8/readme.md
@@ -0,0 +1,1 @@
+Selecting what kind of CPU.
